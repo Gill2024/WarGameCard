a6754ebd4c3a2c9934745be0be3017c2c5539970
Gill2024 <gill.andreyev@gmail.com>
2024-05-28 16:55:39 +0300

    cards class finshed

---
+++ game_cards/Card.py
@@ -0,0 +1,54 @@
+class Card:
+
+    def __init__(self,value,suit):
+
+        self.suit = suit
+        self.suits_dict = {1: '♦', 2: '♠', 3: '♥', 4: '♣'}
+        # 1 clubs (♣),2 diamonds (♦),3 hearts (♥) 4 spades (♠)
+        self.value = value
+        self.values_dict = {1: 'A', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6',
+                            7:'7', 8:'8', 9:'9', 10:'10', 11:'J', 12:'Q', 13:'K'}
+
+
+    def __str__(self):
+        return f'{self.values_dict[self.value]} {self.suits_dict[self.suit]}'
+
+    def __repr__(self):
+        return f' {self.values_dict[self.value]} {self.suits_dict[self.suit]}'
+
+    def __gt__(self, other):
+        '''Method that decides who's card have grater value '''
+        if self.value == other.value:
+            return self.suit > other.suit
+        elif self.value == 1:
+            return True
+        elif other.value == 1:
+            return False
+        else:
+            return self.value > other.value
+
+
+    def __eq__(self, other):
+        if self.value == other.value:
+            if self.suit == other.suit:
+                return True
+        elif self.suit == other.suit:
+            if self.value == other.value:
+                return True
+        else:
+            return False
+
+
+# 1 clubs (♣),2 diamonds (♦),3 hearts (♥) 4 spades (♠)
+card1=Card(2,1)
+card2=Card(5,2)
+card3=Card(1,1)
+card4=Card(1,2)
+card5=Card(10,2)
+
+print(f'1:[{card1}] 2:[{card2}] 3:[{card3}] 4:[{card4}] 5:[{card5}]')
+print(card3.__gt__(card5))
+print(card3.__gt__(card1))
+print(card5.__eq__(card5))
+print(card3.__gt__(card1))
+
---
+++ game_cards/DeckOfCards.py
@@ -0,0 +1,10 @@
+from Card import Card
+
+class DeckOfCards:
+
+    def __init__(self):
+        self.full_deck = []
+        for i in self.__init__(values_dict):
+
+
+
